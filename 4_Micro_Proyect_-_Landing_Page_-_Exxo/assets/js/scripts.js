cd1d76e7ddc13afd1cc86fac021df80dd6802d2d
Juan Pablo <devecchi@exxo.es>
2020-05-03 18:44:45 +0200

    UI forms

--- 4_Micro_Proyect_-_Landing_Page_-_Exxo/assets/js/scripts.js
+++ 4_Micro_Proyect_-_Landing_Page_-_Exxo/assets/js/scripts.js
@@ -1,45 +1,22 @@
-jQuery(document).ready(function () {
-  /*
-	    Navigation
-	*/
-  $('a.scroll-link').on('click', function (e) {
-    e.preventDefault();
-    scroll_to($(this), $('nav').outerHeight());
-  });
-});
-
-// VER COMO CAMBIAR EL COLOR DE FONDE DEL MENÚ
-
+// Cambiar el color del navbar
 /*
-// Hide / Show menu
-var menuHeight = $('#menu_bg').outerHeight();
-$('#menu_bg').css('margin-top', '-' + menuHeight); // Hide menu when page load
-$('#menu_bg').css('display', 'block');
 
-$('#burger').click(function () {
-  // close
-  if ($(this).hasClass('open')) {
-    $('#menu_bg').animate(
-      {
-        marginTop: -menuHeight,
-      },
-      500,
-      function () {
-        // Animation complete.
-      }
-    );
-  }
-  // open
-  else {
-    $('#menu_bg').animate(
-      {
-        margin: 0,
-      },
-      500,
-      function () {
-        // Animation complete.
-      }
-    );
-  }
+jQuery(document).ready(function () {
+  $('#navbarNav').has('.show', function () {
+    $('#principalnabvar')
+      .css('background-color', '') // borro el background actual
+      .css('background-color', 'black'); //asigno el background color
+  });
+  function () 
 });
+
+
+jQuery(document).ready(function () {
+$('#navbarNav').has('.show', function(){
+  $('.hidden-content').removeClass('hidden');
+  $('#principalnabvar').removeClass('bg-white'); // this probably not needed, but use it to remove the previous background class
+  $('.hidden-content').addClass('show');
+  $('#principalnabvar').addClass('.bg-dark');  // adds the new class with the new background color
+});
+
 */
